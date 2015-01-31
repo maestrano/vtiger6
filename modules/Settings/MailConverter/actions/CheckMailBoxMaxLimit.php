eddd93d6d14e38dd7c681cc7a3eb4d2fe2fbe860
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-01-31 18:05:27 +1100

    vtiger 6.2.0

---
+++ modules/Settings/MailConverter/actions/CheckMailBoxMaxLimit.php
@@ -0,0 +1,26 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.1
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+class Settings_MailConverter_CheckMailBoxMaxLimit_Action extends Settings_Vtiger_Index_Action {
+	
+	public function process(Vtiger_Request $request) {
+		$recordsCount = Settings_MailConverter_Record_Model::getCount();
+		$qualifiedModuleName = $request->getModule(false);
+		$response = new Vtiger_Response();
+		if ($recordsCount < 2) {
+			$result = array(true);
+			$response->setResult($result);
+		} else {
+			$response->setError(vtranslate('LBL_MAX_LIMIT_ONLY_TWO', $qualifiedModuleName));
+		}
+		$response->emit();
+	}
+}
+?>
