717b0cc5811c8cbca1f92eddfc8ebe5ef6aff4d3
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-04-19 19:39:25 +1000

    Map PurchaseOrders

--- maestrano/connec/PurchaseOrderMapper.php
+++ maestrano/connec/PurchaseOrderMapper.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
-* Map Connec Supplier Invoice representation to/from vTiger PurchaseOrder
+* Map Connec PurchaseOrder representation to/from vTiger PurchaseOrder
 */
 class PurchaseOrderMapper extends TransactionMapper {
   private $po_status_mapping = null;
@@ -10,18 +10,13 @@ class PurchaseOrderMapper extends TransactionMapper {
   public function __construct() {
     parent::__construct();
 
-    $this->connec_entity_name = 'Invoice';
+    $this->connec_entity_name = 'PurchaseOrder';
     $this->local_entity_name = 'PurchaseOrder';
-    $this->connec_resource_name = 'invoices';
-    $this->connec_resource_endpoint = 'invoices';
+    $this->connec_resource_name = 'purchase_orders';
+    $this->connec_resource_endpoint = 'purchase_orders';
 
     $this->po_status_mapping = array('DRAFT' => 'Created', 'AUTHORISED' => 'Approved', 'DELIVERED' => 'Delivered', 'CANCELLED' => 'Cancelled', 'RECEIVED' => 'Received Shipment');
     $this->po_status_mapping_reverse = array('Created' => 'DRAFT', 'Approved' => 'AUTHORISED', 'Delivered' => 'DELIVERED', 'Cancelled' => 'CANCELLED', 'Received Shipment' => 'RECEIVED');
-  }
-
-  protected function validate($purchase_order_hash) {
-    // Process only Customer PurchaseOrders
-    return $purchase_order_hash['type'] == 'SUPPLIER';
   }
 
   // Map the Connec resource attributes onto the vTiger PurchaseOrder
@@ -41,9 +36,6 @@ class PurchaseOrderMapper extends TransactionMapper {
   // Map the vTiger PurchaseOrder to a Connec resource hash
   protected function mapModelToConnecResource($purchase_order) {
     $purchase_order_hash = parent::mapModelToConnecResource($purchase_order);
-
-    // Default invoice type to SUPPLIER on creation
-    $purchase_order_hash['type'] = 'SUPPLIER';
 
     // Map attributes
     if($this->is_set($purchase_order->column_fields['requisition_no'])) { $purchase_order_hash['transaction_number'] = $purchase_order->column_fields['requisition_no']; }
