4dcad1e9c47504994016f87fe06d6a6c7666f401
BrunoChauvet <bruno.chauvet@live.fr>
2015-05-17 13:43:16 +1000

    Map event status

--- maestrano/connec/EventMapper.php
+++ maestrano/connec/EventMapper.php
@@ -11,6 +11,9 @@ class EventMapper extends BaseMapper {
     $this->local_entity_name = 'EventManagement';
     $this->connec_resource_name = 'events';
     $this->connec_resource_endpoint = 'events';
+
+    $this->event_status_mapping = array('DRAFT' => 'Draft', 'LIVE' => 'Live', 'COMPLETED' => 'Closed');
+    $this->event_status_mapping_reverse = array('Draft' => 'DRAFT', 'Live' => 'LIVE', 'Closed' => 'COMPLETED');
   }
 
   // Return the Event local id
@@ -34,12 +37,13 @@ class EventMapper extends BaseMapper {
     if($this->is_set($event_hash['code'])) { $event->column_fields['event_number'] = $event_hash['code']; }
     if($this->is_set($event_hash['name'])) { $event->column_fields['event_name'] = $event_hash['name']; }
     if($this->is_set($event_hash['description'])) { $event->column_fields['description'] = $event_hash['description']; }
-    if($this->is_set($event_hash['status'])) { $event->column_fields['event_status'] = $event_hash['status']; }
     if($this->is_set($event_hash['url'])) { $event->column_fields['url'] = $event_hash['url']; }
     if($this->is_set($event_hash['start_date'])) { $event->column_fields['start_date'] = $this->format_date_to_php($event_hash['start_date']); }
     if($this->is_set($event_hash['end_date'])) { $event->column_fields['end_date'] = $this->format_date_to_php($event_hash['end_date']); }
     if($this->is_set($event_hash['capacity'])) { $event->column_fields['capacity'] = $event_hash['capacity']; }
     if($this->is_set($event_hash['currency'])) { $event->column_fields['currency'] = $event_hash['currency']; }
+    
+    if($this->is_set($event_hash['status'])) { $event->column_fields['event_status'] = $this->event_status_mapping[$event_hash['status']]; }
   }
 
   // Map the vTiger Event to a Connec resource hash
@@ -56,6 +60,8 @@ class EventMapper extends BaseMapper {
     if($this->is_set($event->column_fields['end_date'])) { $event_hash['end_date'] = $this->format_date_to_connec($event->column_fields['end_date']); }
     if($this->is_set($event->column_fields['capacity'])) { $event_hash['capacity'] = $event->column_fields['capacity']; }
     if($this->is_set($event->column_fields['currency'])) { $event_hash['currency'] = $event->column_fields['currency']; }
+
+    if($this->is_set($event->column_fields['event_status'])) { $event_hash['status'] = $this->event_status_mapping_reverse[$event->column_fields['event_status']]; }
 
     return $event_hash;
   }
