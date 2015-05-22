b5d8774b53ed3365e6d2d2bf8a3a3f3e39443bd7
BrunoChauvet <bruno.chauvet@live.fr>
2015-05-22 15:05:50 +1000

    Rename Organizations to Memberships and Opportunity to Sponsorship

--- languages/en_us/Accounts.php
+++ languages/en_us/Accounts.php
@@ -8,9 +8,9 @@
  * All Rights Reserved.
  *************************************************************************************/
 $languageStrings = array(
-	'Account Name' => 'Organization Name',
-	'Account No' => 'Organization Number',
-	'Accounts' => 'Organizations',
+	'Account Name' => 'Membership Name',
+	'Account No' => 'Membership Number',
+	'Accounts' => 'Memberships',
 	'Analyst'=>'Analyst',
 	'Competitor'=>'Competitor',
 	'Customer'=>'Customer',
@@ -19,14 +19,14 @@ $languageStrings = array(
 	'industry' => 'Industry',
 	'Integrator'=>'Integrator',
 	'Investor'=>'Investor',
-	'LBL_ACCOUNT_INFORMATION' => 'Organization Details',
-	'LBL_ADD_RECORD' => 'Add Organization',
+	'LBL_ACCOUNT_INFORMATION' => 'Membership Details',
+	'LBL_ADD_RECORD' => 'Add Membership',
 	'LBL_COPY_BILLING_ADDRESS' => 'Copy Billing Address',
 	'LBL_COPY_SHIPPING_ADDRESS' => 'Copy Shipping Address',
-	'LBL_DUPLICATES_EXIST' => 'Organization Name already exists',
+	'LBL_DUPLICATES_EXIST' => 'Membership Name already exists',
 	'LBL_END_DATE' => 'End Date',
-	'LBL_RECORDS_LIST' => 'Organization List',
-	'LBL_SHOW_ACCOUNT_HIERARCHY' => 'Organization Hierarchy',
+	'LBL_RECORDS_LIST' => 'Membership List',
+	'LBL_SHOW_ACCOUNT_HIERARCHY' => 'Membership Hierarchy',
 	'LBL_START_DATE' => 'Start Date',
 	'Member Of' => 'Member Of',
 	'Other Email' => 'Secondary Email',
@@ -37,14 +37,14 @@ $languageStrings = array(
 	'Prospect'=>'Prospect',
 	'Reseller'=>'Reseller',
 	'SIC Code' => 'SIC Code',
-	'SINGLE_Accounts' => 'Organization',
+	'SINGLE_Accounts' => 'Membership',
 	'Ticker Symbol' => 'Ticker Symbol',
 	'Website' => 'Website',
 );
 
 $jsLanguageStrings = array(
-	'JS_DUPLICTAE_CREATION_CONFIRMATION' => 'Organization Name already Exists.Do you want to create a duplicate record?',
-	'LBL_DELETE_CONFIRMATION' => 'Deleting this Organization will remove its related Opportunities & Quotes. Are you sure you want to delete this Organization?',
-	'LBL_MASS_DELETE_CONFIRMATION' => 'Deleting this Organization(s) will remove its related Opportunities & Quotes. Are you sure you want to delete the selected records?',
+	'JS_DUPLICTAE_CREATION_CONFIRMATION' => 'Membership Name already Exists.Do you want to create a duplicate record?',
+	'LBL_DELETE_CONFIRMATION' => 'Deleting this Membership will remove its related Opportunities & Quotes. Are you sure you want to delete this Membership?',
+	'LBL_MASS_DELETE_CONFIRMATION' => 'Deleting this Membership(s) will remove its related Opportunities & Quotes. Are you sure you want to delete the selected records?',
 	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Are you sure you want to delete?',
 );
