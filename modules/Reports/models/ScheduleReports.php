17606deaefba21138b6b84e1c7a778c2f2f1c854
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-09-22 17:10:20 +1000

    Remove default CC recipient for ScheduledReports

--- modules/Reports/models/ScheduleReports.php
+++ modules/Reports/models/ScheduleReports.php
@@ -210,8 +210,10 @@ class Reports_ScheduleReports_Model extends Vtiger_Base_Model {
 			$vtigerMailer->AddAttachment($path, decode_html($attachmentName));
 		}
 		//Added cc to account owner
-		$accountOwnerId = Users::getActiveAdminId();
-		$vtigerMailer->AddCC(getUserEmail($accountOwnerId), getUserFullName($accountOwnerId));
+		// Maestrano - Do not add the first admin as default CC recipient
+		// $accountOwnerId = Users::getActiveAdminId();
+		// $vtigerMailer->AddCC(getUserEmail($accountOwnerId), getUserFullName($accountOwnerId));
+
 		$status = $vtigerMailer->Send(true);
 
 		foreach ($attachments as $attachmentName => $path) {
